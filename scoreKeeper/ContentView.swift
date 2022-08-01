b1d0cf09fa9cc90528b6f5702f1b15d1dec393c0
Jaden Scali <jadenscali132@gmail.com>
2022-08-01 09:24:38 -0400

    Initial Commit

---
+++ scoreKeeper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  scoreKeeper
+//
+//  Created by Jaden Scali on 2022-08-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ scoreKeeper/scoreKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  scoreKeeperApp.swift
+//  scoreKeeper
+//
+//  Created by Jaden Scali on 2022-08-01.
+//
+
+import SwiftUI
+
+@main
+struct scoreKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
